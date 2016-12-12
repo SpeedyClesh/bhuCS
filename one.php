39e22cf7afcaa3a2eb4ff61c5bb29bdd684fa639
Samuel Theophilus <samtheo1597@outlook.com>
2016-12-12 13:00:30 +0100

    Update on one.php
    Structured template for One.php file

---
+++ one.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	include('inc/header.php');
+?>
+<style type="text/css">
+	p{
+		text-align: justify;
+	}
+</style>
+
+<!-- Main -->
+				<div id="main-wrapper">
+					<div id="main" class="container">
+						<div class="row">
+							<div class="12u">
+								<div class="content">
+
+									<!-- Content -->
+										<section class="box features">
+											<h2 class="major"><span>100 LEVEL</span></h2>
+							<p><strong>FIRST SEMESTER<strong></p>
+						
+						<div class="table-responsive">
+							<table border="8">
+								<tr>
+									<td>N.o.</td>
+									<td>COURSES</td>
+									<td>TITLE</td>
+									<td>CREDIT UNIT</td>
+								</tr>
+								<?php
+								 
+								?>
+								<tr>
+									<td></td>
+									<td></td>
+									<td>Total</td>
+									<td>22</td>
+								</tr>
+							</table>
+							
+							<p><strong>SECOND SEMESTER<strong></p>
+						
+						<div class="table-responsive">
+							<table border="8">
+								<tr>
+									<td>N.o.</td>
+									<td>COURSES</td>
+									<td>TITLE</td>
+									<td>CREDIT UNIT</td>
+								</tr>
+								<tr>
+									<td>1.</td>
+									<td>CMP102</td>
+									<td>Computer System Principles</td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td>2.</td>
+									<td>CMP104</td>
+									<td>Programming in C</td>
+									<td>3</td>
+								</tr>
+								<tr>
+									<td>3.</td>
+									<td>MTH102</td>
+									<td>Elementary Mathematics II </td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td>4.</td>
+									<td>CISCO</td>
+									<td>CCNA 1</td>
+									<td>0</td>
+								</tr>
+								<tr>
+									<td>5.</td>
+									<td>PHY108</td>
+									<td>Physics Lab II</td>
+									<td>1</td>
+								</tr>
+								<tr>
+									<td>6.</td>
+									<td>PHY102</td>
+									<td>General Physics II</td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td>7.</td>
+									<td>GST102</td>
+									<td>Philosophy & Logic</td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td>8.</td>
+									<td>GST122</td>
+									<td>Communication in English II</td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td>9.</td>
+									<td>GST124</td>
+									<td>Communication in French or Arabic</td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td>10.</td>
+									<td>BST102</td>
+									<td>Christian Belief II</td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td>11.</td>
+									<td>CMP110</td>
+									<td>Intro to Problem Solving</td>
+									<td>2</td>
+									
+								</tr>
+								<tr>
+									<td>12.</td>
+									<td>CMP106</td>
+									<td>Intro to Hardware/Software Maintenance</td>
+									<td>2</td>
+									
+								</tr>
+								<tr>
+									<td></td>
+									<td></td>
+									<td>Total</td>
+									<td>21</td>
+								</tr>
+							</table>
+							</section>
+			</section>
+							
+						</div>
+					</div>
+				</div>
+						
+						<div class="row 200%">
+							<div class="12u">
+		<!-- Features -->
+									<section class="box features">
+										<h2 class="major"><span>*NOTE*</span></h2>
+										<div>
+											<div class="row">
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															
+															
+															<p>
+																1.	It is Mandatory for Computer Science students to complete the CISCO
+																certifications (CCNA 1 to CCNA2) before graduation.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<p>
+																2.	Students are required to write A+ certification Examination (external). There
+																will be no internal Exam for CMP 10 and CMP107.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															
+															<p>
+																3. Students are reuired to enhance thier French or Arabic Language skills by 
+																registering for and obtaining external certification before graduation.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															
+															<p>
+																4. Certificates will be issued to all successfull students in CMP103 
+																Examination.
+															</p>
+										</section>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+</body>
+<?php
+	include('inc/footer2.php');
+?>
+</html>
